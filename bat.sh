714a7d7d160f63dae7264f7c3f0b45953fdd2b8d
Tiphanie Dousset <tiphdousset@gmail.com>
2016-05-13 17:23:01 +0200

    add a check to verify if computer is charging

--- bat.sh
+++ bat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export DISPLAY=:0
-bat="upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//i"
-p=$(eval $bat)
-if [ "$p" -lt "10" ]; then
+bat=$(upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//i)
+state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E state|xargs|cut -d' ' -f2|sed s/%//i)
+if [[ "$bat" -lt "10" ]] && [[ "$state" == "discharging" ]]; then
 	notify-send 'Attention!' 'Battery is less than 10%' --icon=dialog-information
 fi
