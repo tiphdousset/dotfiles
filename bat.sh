2fec7b2c0fe0a24e0664f8d1f48a453140f03e40
Tiphanie Dousset <tiphdousset@gmail.com>
2016-07-04 22:58:20 +0200

    Displays the current percentage

--- bat.sh
+++ bat.sh
@@ -3,5 +3,5 @@ export DISPLAY=:0
 bat=$(upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//i)
 state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E state|xargs|cut -d' ' -f2|sed s/%//i)
 if [[ "$bat" -lt "10" ]] && [[ "$state" == "discharging" ]]; then
-	notify-send 'Attention!' 'Battery is less than 10%' --icon=dialog-information
+	notify-send "Attention!" "Battery is less than ${bat}%" --icon=dialog-information
 fi
