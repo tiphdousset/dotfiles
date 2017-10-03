af0236acd7855b7ce73f164d8650444b8cd649c6
Tiphanie Dousset <tiphdousset@gmail.com>
2017-10-03 18:05:52 +0200

    Play a sound instead of displaying a notification

--- bat.sh
+++ bat.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-export DISPLAY=:0
+#export DISPLAY=:0
 bat=$(upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//i)
 state=$(upower -i $(upower -e | grep BAT) | grep --color=never -E state|xargs|cut -d' ' -f2|sed s/%//i)
+
+DBUS_SESSION_BUS_ADDRESS=`grep "DBUS_SESSION_BUS_ADDRESS=" /home/tiphanie/.dbus/session-bus/*-0 | cut -d "=" -f 2-`
+export DISPLAY=:0
+
 if [[ "$bat" -lt "10" ]] && [[ "$state" == "discharging" ]]; then
-	notify-send "Attention!" "Battery is less than ${bat}%" --icon=dialog-information
+#DISPLAY=${DISPLAY} DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDRESS} notify-send "Attention!" "Battery is less than ${bat}%" --icon=dialog-information
+    XDG_RUNTIME_DIR=/run/user/1000 espeak battery > /home/tiphanie/error.log 2>&1
 fi
