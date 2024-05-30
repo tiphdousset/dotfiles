90313dcb04cbe724067075f441f369ffab62cea0
Tiphanie <tiphanie.dousset@hivemindtechnologies.com>
2024-05-30 16:33:40 +0200

    Add new vim plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,6 +67,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+ -- restore cursor position in vim
+  { "ethanholz/nvim-lastplace", config = true },
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
